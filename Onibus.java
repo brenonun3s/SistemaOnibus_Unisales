3c5c37256b34ea514fbe09f30773924233881729
Breno Nunes <166417202+brenonun3s@users.noreply.github.com>
2024-09-29 11:20:38 -0300

    Add files via upload
    Atualização do código, juntando os metodos desenvolvidos por Gabriel Ghroner e Breno Nunes

--- Onibus.java
+++ Onibus.java
@@ -1,62 +1,112 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Onibus {
-  
-    private int numero_Onibus;
-    private int[] quantidadePassageiros = new int[40];
-    private int[] assentos = new int[40];
 
- /*Classe Onibus
-public class Onibus {
-    private String placa;
+    private int numeroOnibus;
+    private List<Integer> quantidadePassageiros = new ArrayList<>(40);
+    private List<Boolean> assentos = new ArrayList<>(40);
+    private String placaOnibus;
+    private int assentoEscolhido;
+    private List<String> passageiros = new ArrayList<>();
 
-    public Onibus(String placa) {
-        this.placa = placa;
+    public Onibus() {
+        for (int i = 0; i < 40; i++) {
+            assentos.add(false);
+            quantidadePassageiros.add(0);
+        }
     }
 
-    public boolean validarPlaca() {
-        return placa.length() <= 4;
+    public int getNumeroOnibus() {
+        return numeroOnibus;
     }
 
-    public String getPlaca() {
-        return placa;
+    public void setNumeroOnibus(int numeroOnibus) {
+        this.numeroOnibus = numeroOnibus;
     }
 
-    public void setPlaca(String placa) {
-        this.placa = placa;
-    }
-}
- /* */
-
-
-
-
-/*public class Onibus {
-    private String placa;
-
-    Esse codigo aqui deve ser usado na classe main ****** ESTOU CONSTRUINDO ******
-
-    public Onibus(String placa) {
-        this.placa = placa;
+    public List<Integer> getQuantidadePassageiros() {
+        return quantidadePassageiros;
     }
 
-    public boolean validarPlaca() {
-        return placa.length() <= 4;
+    public void setQuantidadePassageiros(List<Integer> quantidadePassageiros) {
+        this.quantidadePassageiros = quantidadePassageiros;
     }
 
-    public String getPlaca() {
-        return placa;
+    public String getPlacaOnibus() {
+        return placaOnibus;
     }
 
-    public void setPlaca(String placa) {
-        this.placa = placa;
+    public void setPlacaOnibus(String placaOnibus) {
+        this.placaOnibus = placaOnibus;
     }
 
-}
-    /* */
+    public List<Boolean> getAssentos() {
+        return assentos;
+    }
+
+    public void setAssentos(List<Boolean> assentos) {
+        this.assentos = assentos;
+    }
+
+    public int getAssentoEscolhido() {
+        return assentoEscolhido;
+    }
+
+    public void escolherAssento() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, digite o número do assento (1-40): ");
+        int assentoEscolhido = scanner.nextInt();
+
+        if (assentoEscolhido < 1 || assentoEscolhido > 40) {
+            System.out.println("Número Inválido! Por favor, escolha um número de 1 a 40");
+            return;
+        }
+
+        if (assentos.get(assentoEscolhido - 1)) {
+            System.out.println("O assento " + assentoEscolhido + " já está ocupado!");
+        } else {
+            assentos.set(assentoEscolhido - 1, true);
+            this.assentoEscolhido = assentoEscolhido;
+            System.out.println("Assento " + assentoEscolhido + " reservado com sucesso!");
+        }
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Olá Caro Passageiro! Segue dados da sua viagem:");
+        System.out.println("Placa: " + getPlacaOnibus());
+        System.out.println("Seu assento é: " + getAssentoEscolhido());
+
+    }
 
 
+    public void adicionarPassageiro(String nome) {
+            passageiros.add(nome);
+            System.out.println("Passageiro " + nome + " adicionado com sucesso!");
+        }
 
+    public void removerPassageiro(String nome) {
+            if (passageiros.remove(nome)) {
+                System.out.println("Passageiro " + nome + " removido com sucesso!");
+            } else {
+                System.out.println("Passageiro " + nome + " não encontrado.");
+            }
+        }
 
+    public void listarPassageiros() {
+            System.out.println("Lista de passageiros:");
+            for (String passageiro : passageiros) {
+                System.out.println(passageiro);
+            }
+        }
 
-    
-}
+    public List<String> getPassageiros() {
+            return passageiros;
+        }
+
+    public void setPassageiros(List<String> passageiros) {
+            this.passageiros = passageiros;
+        }
+    }
+
