8b353a7fec80b5042ccba9469e0d242c7e3fe260
Rafael.Gogge <rafa.gogge@gmail.com>
2024-09-30 23:23:28 -0300

    att

--- rafael/Main.java
+++ rafael/Main.java
@@ -2,11 +2,11 @@ package rafael;
 
 public class Main {
         public static void main(String[] args) {
-                // UsuarioAdmin admin = new UsuarioAdmin(1, "Rafael", 24, "12345678910",
-                // "rafael@teste.com");
+                UsuarioAdmin admin = new UsuarioAdmin(1, "Rafael", 24, "12345678910",
+                "rafael@teste.com");
 
-                // UsuarioPassageiro passageiro = new UsuarioPassageiro(2, "Jamal", 25,
-                // "987.654.321-00", "ana@passageiro.com");
+                UsuarioPassageiro passageiro = new UsuarioPassageiro(2, "Jamal", 25,
+                "987.654.321-00", "ana@passageiro.com");
 
         }
 }
