e7ebc3b07b68f978a47162929ec64ebc51ae1de2
Rafael.Gogge <rafa.gogge@gmail.com>
2024-10-01 22:32:24 -0300

    att

--- Main.java
+++ Main.java
@@ -13,14 +13,15 @@ public class Main {
         //laranjeiras.listarPassageiros();
         //laranjeiras.removerPassageiro("Breno");
         //laranjeiras.listarPassageiros();
+        
         laranjeiras.setNumeroOnibus("1234");
         System.out.println(laranjeiras.getNumeroOnibus());
         
-        UsuarioAdmin admin = new UsuarioAdmin(1, "Rafael", 24, "12345678910",
-        "rafael@teste.com");
+        UsuarioAdmin admin = new UsuarioAdmin(1, "Rafael", 24, "123.456.789-00",
+        "rafael@admin.com");
 
         UsuarioPassageiro passageiro = new UsuarioPassageiro(2, "Jamal", 25,
-        "987.654.321-00", "ana@passageiro.com");
+        "987.654.321-00", "jamal@passageiro.com");
 
         admin.alterarAssento(2, passageiro);
         passageiro.alterarAssentoPassageiro(6);
